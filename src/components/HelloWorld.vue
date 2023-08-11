3098e5182e797858dbf7435b567913ad3e0d7b53
Simon Schwertner <simonjulius.schwertner@student.hs-rm.de>
2023-08-11 21:42:51 +0200

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+
+      <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
+
+      <h1 class="text-h2 font-weight-bold">Simon's Site</h1>
+
+      <div class="py-14" />
+
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
